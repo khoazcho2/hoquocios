5bf88b07301963ae820b871166fdb41de15396ef
khoazcho2 <vot835848@gmail.com>
2026-09-08 10:02:11 +0700

    fix: Move Swift source files to correct Xcode target directory

---
+++ FreeFireMaxProxyUI/FreeFireMaxProxyUI/FreeFireMaxProxyUIApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct FreeFireMaxProxyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
